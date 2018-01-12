a13531976f215e6fd0160dd336627e1f1777ce7e
Andrew Kowalcyk <andrewrkowalczyk@gmail.com>
2018-01-12 18:09:30 +0100

    changes:
    * add react-flexbox-grid for a nice grid system
    * add basic structure of the caluclator to Calculator.js
    * add sourcemap to webpack config for clearer stacktraces

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
-class Calculator extends Component {
+const createRowSizeObject = (sizes, width, partition) =>
+  sizes.reduce((acc, size) => {
+      acc[size] = width * partition;
+      return acc;
+    }, {});
+
+export default class Calculator extends Component {
   render () {
+    const width = 12;
+    const sizes = ['xs', 'sm', 'md', 'lg'];
+    const fullRow = createRowSizeObject(sizes, width, 1);
+    const fourthRow = createRowSizeObject(sizes, width, 1/4);
+    const halfRow = createRowSizeObject(sizes, width, 1/2);
+
     return (
-      <div>Hello world!</div>
+      <Grid fluid>
+        <Row>
+          <Col {...fullRow}>
+            Display for calculators result
+          </Col>
+        </Row>
+        <Row>
+          <Col {...fourthRow}>C</Col>
+          <Col {...fourthRow}>+/-</Col>
+          <Col {...fourthRow}>%</Col>
+          <Col {...fourthRow}>/</Col>
+        </Row>
+        <Row>
+          <Col {...fourthRow}>7</Col>
+          <Col {...fourthRow}>8</Col>
+          <Col {...fourthRow}>9</Col>
+          <Col {...fourthRow}>x</Col>
+        </Row>
+        <Row>
+          <Col {...fourthRow}>4</Col>
+          <Col {...fourthRow}>5</Col>
+          <Col {...fourthRow}>6</Col>
+          <Col {...fourthRow}>-</Col>
+        </Row>
+        <Row>
+          <Col {...fourthRow}>1</Col>
+          <Col {...fourthRow}>2</Col>
+          <Col {...fourthRow}>3</Col>
+          <Col {...fourthRow}>+</Col>
+        </Row>
+        <Row>
+          <Col {...halfRow}>0</Col>
+          <Col {...fourthRow}>.</Col>
+          <Col {...fourthRow}>=</Col>
+        </Row>
+      </Grid>
     );
   }
 }
-
-export default Calculator;
