d76ba94123347b7089f3c742728d271f6932c527
Andrew Kowalcyk <andrewrkowalczyk@gmail.com>
2018-01-12 16:25:58 +0100

    create the inital Calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Calculator extends Component {
+  render () {
+    return (
+      <div>
+        Hello world!
+      </div>
+    );
+  }
+}
