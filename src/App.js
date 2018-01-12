ea45a2d1019184ed0afc48a07dc51af165a5d196
Andrew Kowalcyk <andrewrkowalczyk@gmail.com>
2018-01-12 16:25:58 +0100

    create the inital Calculator component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h1>Welcome to React</h1>
-        </header>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Calculator from './components/Calculator';
+
+const App = () => <Calculator />;
 
 export default App;
