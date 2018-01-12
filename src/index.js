dc4ac06d3ec778e36eb5381fda991f285ba83cd2
Andrew Kowalcyk <andrewrkowalczyk@gmail.com>
2018-01-12 18:38:18 +0100

    add redux & react-redux, wire up app to the app's reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import calculatorReducer from './reducers/calculatorReducer';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStore(calculatorReducer)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
---
+++ src/reducers/calculatorReducer.js
@@ -0,0 +1,6 @@
+export default function calculator (state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
